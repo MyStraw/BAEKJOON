733b1f32c14f53103459c5f396c50da0bb9dbe5d
MyStraw <129818873+MyStraw@users.noreply.github.com>
2023-09-02 23:32:51 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 236 ms, Memory: 18344 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int[] a = new int[6];
+		int b = 0;
+
+		a[0] = 1;
+		a[1] = 1;
+		a[2] = 2;
+		a[3] = 2;
+		a[4] = 2;
+		a[5] = 8;
+
+		int chess = 0;
+		for (int i = 0; i < 6; i++) {
+			b = sc.nextInt();
+			System.out.print((chess = a[i] - b) + " ");
+
+		}
+
+	}
+
+}
