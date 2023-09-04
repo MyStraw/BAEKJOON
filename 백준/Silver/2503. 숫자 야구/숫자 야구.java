395412d5d971a973375fa24ad8281cc6479d9fb0
MyStraw <129818873+MyStraw@users.noreply.github.com>
2023-09-04 22:41:25 +0900

    [Silver III] Title: 숫자 야구, Time: 140 ms, Memory: 15728 KB -BaekjoonHub

---
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));		
+		int N = Integer.parseInt(br.readLine());
+		List<Integer> aa = new ArrayList<>();
+		List<Integer> bb = new ArrayList<>();
+		for (int i = 100; i <= 999; i++) {
+			if (Integer.toString(i).charAt(0) != Integer.toString(i).charAt(1)
+					&& Integer.toString(i).charAt(1) != Integer.toString(i).charAt(2)
+					&& Integer.toString(i).charAt(0) != Integer.toString(i).charAt(2)
+					&& Integer.toString(i).charAt(1) != '0' && Integer.toString(i).charAt(2) != '0') {
+				aa.add(i);
+			}
+		}		
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			String num = st.nextToken();			
+			int strike = Integer.parseInt(st.nextToken());
+			int ball = Integer.parseInt(st.nextToken());
+			
+			
+			for (int c : aa) {
+				int strikecount = 0;
+				int ballcount = 0;
+				if (Integer.toString(c).charAt(0) == num.charAt(0)) {
+					strikecount++;
+				}
+				if (Integer.toString(c).charAt(1) == num.charAt(1)) {
+					strikecount++;
+				}
+				if (Integer.toString(c).charAt(2) == num.charAt(2)) {
+					strikecount++;
+				}				
+				if (Integer.toString(c).charAt(0) == num.charAt(1)) {
+					ballcount++;
+				}
+				if (Integer.toString(c).charAt(0) == num.charAt(2)) {
+					ballcount++;
+				}
+				if (Integer.toString(c).charAt(1) == num.charAt(0)) {
+					ballcount++;
+				}
+				if (Integer.toString(c).charAt(1) == num.charAt(2)) {
+					ballcount++;
+				}
+				if (Integer.toString(c).charAt(2) == num.charAt(0)) {
+					ballcount++;
+				}				
+				if (Integer.toString(c).charAt(2) == num.charAt(1)) {
+					ballcount++;
+				}
+				
+				if (strike == strikecount && ball == ballcount) {
+					bb.add(c);
+				}				
+			}
+			aa = new ArrayList<>(bb);
+			bb.clear();			
+		}
+		bw.write(String.valueOf(aa.size()));
+		bw.flush();
+		bw.close();
+	}
+}
