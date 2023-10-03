f754dd06e57533a8ff81340d558ce99d07595815
MyStraw <129818873+MyStraw@users.noreply.github.com>
2023-10-03 12:47:40 +0900

    [Bronze I] Title: 이항 계수 1, Time: 216 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		int total = 1;
+		int na = 1;
+		
+		for (int i = 1 ; i<=K ; i++) {
+			na = na * i;
+			total = total * (N-i+1);			
+		}
+		
+		System.out.println(total/na);
+	}
+	
+
+	
+
+}
