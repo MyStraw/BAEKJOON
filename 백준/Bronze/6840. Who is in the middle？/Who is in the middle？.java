8090c19a54a91ea65ac2414b2fd8fdf7c4eba6a5
MyStraw <129818873+MyStraw@users.noreply.github.com>
2023-10-14 21:22:01 +0900

    [Bronze V] Title: Who is in the middle?, Time: 208 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/6840. Who is in the middle？/Who is in the middle？.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int first = sc.nextInt();
+		int second = sc.nextInt();
+		int third = sc.nextInt();
+
+		if (first > second) {
+			if (second > third) {
+				System.out.println(second);
+			} else if (first > third) {
+				if (third > second) {
+					System.out.println(third);
+				}
+			} else if (third > first) {
+				System.out.println(first);
+			}
+		} else if (first < second) {
+			if (second < third) {
+				System.out.println(second);
+			} else if (first > third) {
+				System.out.println(first);
+
+			} else if (third > first) {
+				System.out.println(third);
+			}
+
+		}
+	}
+}
