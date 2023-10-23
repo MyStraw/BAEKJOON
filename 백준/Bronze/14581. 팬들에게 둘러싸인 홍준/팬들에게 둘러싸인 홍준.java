45008f188d4784eb868abf2fefeaea849a3988f9
MyStraw <129818873+MyStraw@users.noreply.github.com>
2023-10-23 21:03:47 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 220 ms, Memory: 18236 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String hongjun = sc.next();
+		
+		System.out.println(":fan::fan::fan:");
+		System.out.println(":fan:" + ":"+hongjun+":"+":fan:");
+		System.out.println(":fan::fan::fan:");
+	}
+
+}
