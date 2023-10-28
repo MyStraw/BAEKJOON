9397393122d12ab329cbf8aec0909dd10bb99fa8
MyStraw <129818873+MyStraw@users.noreply.github.com>
2023-10-28 21:00:42 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 196 ms, Memory: 17500 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String school = sc.next();
+		
+		if (school.equals("NLCS")) {
+			System.out.println("North London Collegiate School");
+		}
+		if (school.equals("BHA")) {
+			System.out.println("Branksome Hall Asia");
+		}
+		if (school.equals("KIS")) {
+			System.out.println("Korea International School");
+		}
+		if (school.equals("SJA")) {
+			System.out.println("St. Johnsbury Academy");
+		}	
+		
+	}
+
+}
