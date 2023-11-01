e41747a6b2b230e6914f96662687a4587db558e8
MyStraw <129818873+MyStraw@users.noreply.github.com>
2023-11-01 20:58:12 +0900

    [Silver I] Title: 하노이 탑, Time: 784 ms, Memory: 85356 KB -BaekjoonHub

---
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+	
+	static StringBuffer str = new StringBuffer();
+
+	static int K = 0;
+	
+	public static void main(String[] args) throws IOException {		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();		
+		if (N <= 20) {
+			Hanoi(N, 1, 2, 3);
+			System.out.println(K);
+			System.out.println(str);
+			
+		} else if (N > 20) {			
+//			BigInteger a = BigInteger.valueOf(2);
+//			BigInteger b = a.pow(N);
+//			BigInteger c = BigInteger.ONE;
+//			BigInteger d = b.subtract(c);
+			System.out.println(new BigInteger("2").pow(N).subtract(BigInteger.ONE));
+		}
+	}
+
+	public static void Hanoi(int Num, int from, int by, int to) throws IOException {
+		if (Num == 1) {
+			str.append(from + " " + to).append("\n");
+			K++;
+		}
+
+		else {
+			Hanoi(Num - 1, from, to, by);
+			K++;
+			str.append(from + " " + to).append("\n");
+			Hanoi(Num - 1, by, from, to);
+
+		}
+	}
+}
