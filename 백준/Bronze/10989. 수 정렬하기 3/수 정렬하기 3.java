6677bdc69b7119b0a15b2958bbdeccbbad9a4111
MyStraw <129818873+MyStraw@users.noreply.github.com>
2023-11-05 14:51:15 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1616 ms, Memory: 336756 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] counts = new int[10001];
+        
+        for (int i = 0; i < N; i++) {
+            counts[Integer.parseInt(br.readLine())]++;
+        }
+        
+        br.close();
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] > 0) {
+                for (int j = 0; j < counts[i]; j++) {
+                    sb.append(i).append('\n');
+                }
+            }
+        }
+        
+        System.out.print(sb);
+    }
+}
