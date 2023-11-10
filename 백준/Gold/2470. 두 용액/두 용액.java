37d7434a2885fdee9e0d6125116b436e3f25a58e
MyStraw <129818873+MyStraw@users.noreply.github.com>
2023-11-10 19:20:40 +0900

    [Gold V] Title: 두 용액, Time: 396 ms, Memory: 28464 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,46 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] N = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int sol1 = 0;
+		int sol2 = 0;
+
+		for (int i = 0; i < n; i++) {
+			N[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(N); // 이거쓰면 sortData 2중포문도, swap도 쓸필요가 없어진다.
+
+		int left = 0;
+		int right = N.length - 1;
+		int sum = 2_000_000_000;
+
+		while (left < right) {
+			if (sum > Math.abs(N[left] + N[right])) {
+				sum = Math.abs(N[left] + N[right]);
+				sol1 = N[left];
+				sol2 = N[right];
+			}
+			if (0 > N[left] + N[right]) {
+				left++;
+			} else {
+				right--;
+			}
+		}
+
+		System.out.print(sol1 + " ");
+		System.out.println(sol2);
+
+	}
+
+}
