18dbf88c672403545087826201cc91c8836b4ca2
MyStraw <129818873+MyStraw@users.noreply.github.com>
2023-11-26 06:37:53 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1772 ms, Memory: 154160 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));		
+		
+
+		int N = Integer.parseInt(br.readLine()) ;
+		int[] su = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			su[i] = Integer.parseInt(br.readLine());
+		}
+
+		Arrays.sort(su);
+
+		for (int r : su) {
+			bw.write(Integer.toString(r)+ "\n");
+		}
+		bw.flush();
+		bw.close();
+	}	
+
+}
+
+
