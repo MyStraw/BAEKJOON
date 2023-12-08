30b6148dc57a14f056ba5a6c9596dacffb0547bf
MyStraw <129818873+MyStraw@users.noreply.github.com>
2023-12-08 20:19:37 +0900

    [Bronze V] Title: 자동완성, Time: 200 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		String al = sc.next();
+		
+		if (al.equals("N") || al.equals("n")) {
+			System.out.println("Naver D2");
+		}
+		else
+			System.out.println("Naver Whale");
+	}
+
+}
