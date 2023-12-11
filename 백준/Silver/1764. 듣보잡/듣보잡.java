5259a9a9fe6843f824565a6af07a5c5e3db98ad2
MyStraw <129818873+MyStraw@users.noreply.github.com>
2023-12-11 22:22:08 +0900

    [Silver IV] Title: 듣보잡, Time: 400 ms, Memory: 29512 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		Map<String, Integer> noListenSee = new HashMap<>();
+
+		for (int i = 0; i < N; i++) {
+			noListenSee.put(br.readLine(), 1);
+		}
+
+		for (int i = 0; i < M; i++) {
+			String name = br.readLine();
+			noListenSee.put(name, noListenSee.getOrDefault(name, 0) + 1);
+		}
+		
+		List<String> nonono = new LinkedList<>();
+		for (Map.Entry<String, Integer> entry : noListenSee.entrySet()) {
+			if(entry.getValue() >=2) {
+				nonono.add(entry.getKey());
+			}
+			
+		}
+		Collections.sort(nonono);
+		
+		bw.write(nonono.size()+"\n");
+	
+		
+		for (String no : nonono) {
+			bw.write(no);
+			bw.newLine();
+		}
+		
+		bw.flush();
+		bw.close();
+
+	}
+
+
+}
