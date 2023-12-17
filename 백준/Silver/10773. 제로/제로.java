f5bd74902534a3d7409e684226ec42f5027c4b50
MyStraw <129818873+MyStraw@users.noreply.github.com>
2023-12-17 21:50:37 +0900

    [Silver IV] Title: 제로, Time: 240 ms, Memory: 23760 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int K = Integer.parseInt(br.readLine());
+
+		Stack<Integer> zero = new Stack<>();
+		int total = 0;
+
+		for (int i = 0; i < K; i++) {
+			int num = Integer.parseInt(br.readLine());
+			if (num != 0) {
+				zero.add(num);
+				total = total + num;
+			} else if (num == 0) {
+				total = total - zero.pop();
+			}
+		}
+		bw.write(String.valueOf(total));
+		bw.flush();
+		bw.close();
+	}
+}
