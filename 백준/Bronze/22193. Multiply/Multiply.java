4542fd9860bc5651a664d6caea46edd45148a976
MyStraw <129818873+MyStraw@users.noreply.github.com>
2023-12-27 16:10:34 +0900

    [Bronze V] Title: Multiply, Time: 212 ms, Memory: 17456 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Bronze/22193. Multiply/Multiply.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int result = N*M;
+		System.out.println(result);
+	}
+}
