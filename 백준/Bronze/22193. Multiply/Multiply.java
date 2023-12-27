fe42b3f30ff8ffacebd0c65d54b60361fd070ce9
MyStraw <129818873+MyStraw@users.noreply.github.com>
2023-12-27 16:14:59 +0900

    [Bronze V] Title: Multiply, Time: 888 ms, Memory: 59692 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/22193. Multiply/Multiply.java
+++ 백준/Bronze/22193. Multiply/Multiply.java
@@ -1,12 +1,13 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
 		int b = sc.nextInt();		
-		long N = sc.nextInt();
-		long M = sc.nextInt();
-		long result = N*M;
+		BigInteger N = sc.nextBigInteger();
+		BigInteger M = sc.nextBigInteger();
+		BigInteger result = N.multiply(M);		
 		System.out.println(result);
 	}
 }
