745d2546fb3ceda0e686cc9e88d2d33464849db6
MyStraw <129818873+MyStraw@users.noreply.github.com>
2024-01-04 15:23:04 +0900

    [Bronze V] Title: 몇개고?, Time: 204 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/27294. 몇개고？/몇개고？.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		 Scanner sc = new Scanner(System.in);
+
+	        int T = sc.nextInt(); 
+	        int S = sc.nextInt();
+	        
+	        int count = riceCount(T,S);
+	        
+	        System.out.println(count);
+		
+	}
+	
+	private static int riceCount(int time, int sool) {
+		if (time>=12 && time <=16 && sool == 0) {
+			return 320;
+		}
+		else {
+			return 280;
+		}
+	}
+
+}
