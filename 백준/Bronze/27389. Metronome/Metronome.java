2dbf92c1268547b1105708db5ab0c01db11c3cea
MyStraw <129818873+MyStraw@users.noreply.github.com>
2024-01-08 17:08:13 +0900

    [Bronze V] Title: Metronome, Time: 216 ms, Memory: 17808 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();		
+		double rot = (double)n/4;		
+		System.out.printf("%.2f\n", rot);
+	}
+}
