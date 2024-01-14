e0af2555ca779b72c4408a72dfbfa8d9a356c1f5
MyStraw <129818873+MyStraw@users.noreply.github.com>
2024-01-14 16:20:11 +0900

    [Bronze V] Title: Lucky 7, Time: 204 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/30224. Lucky 7/Lucky 7.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		String N = String.valueOf(n);
+		boolean contain = N.contains("7");		
+		if(n%7 !=0 && !contain) {
+			System.out.println(0);
+		}
+		else if(n%7 == 0 && !contain) {
+			System.out.println(1);
+		}
+		
+		else if(n%7 != 0 && contain) {
+			System.out.println(2);
+		}
+		else if(n%7 == 0 && contain) {
+			System.out.println(3);
+		}
+	}
+
+}
