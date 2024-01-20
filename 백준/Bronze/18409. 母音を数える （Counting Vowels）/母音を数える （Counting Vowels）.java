5b517bbde984544af08bf7dd9b10d9cdade9065a
MyStraw <129818873+MyStraw@users.noreply.github.com>
2024-01-20 20:30:41 +0900

    [Bronze V] Title: 母音を数える (Counting Vowels), Time: 200 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/18409. 母音を数える （Counting Vowels）/母音を数える （Counting Vowels）.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);	       
+	        int N = Integer.parseInt(sc.nextLine());	       
+	        String s = sc.nextLine();	      
+	        System.out.println(count(s));
+	    }
+	 
+	    private static int count(String s) {
+	        int count = 0;
+	        for (char c : s.toCharArray()) {
+	            if (moem(c)) {
+	                count++;
+	            }
+	        }
+	        return count;
+	    }
+	   
+	    private static boolean moem(char c) {
+	        return "aeiou".indexOf(c) >= 0;
+	    }
+
+}
