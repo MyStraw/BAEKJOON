54d7f1a724007a4b4790f50e84f12e5a0e1be8a5
MyStraw <129818873+MyStraw@users.noreply.github.com>
2024-01-23 15:11:00 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 204 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+
+		int sum = 0;
+		int current = 1;
+		int count = 0;
+
+		for (int i = 1; i <= B; i++) {
+			if (i >= A) {
+				sum += current;
+			}
+
+			count++;
+			if (count == current) {
+				current++;
+				count = 0;
+			}
+		}
+
+		System.out.println(sum);
+	}
+
+}
