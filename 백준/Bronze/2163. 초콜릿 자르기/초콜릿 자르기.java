6fdf8801f1ad18b287bd78060f4436038cfb4929
MyStraw <129818873+MyStraw@users.noreply.github.com>
2024-01-24 10:57:33 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 204 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        int N = sc.nextInt(); 
+	        int M = sc.nextInt();
+	        
+	        int jjo = N * M - 1;
+
+	        System.out.println(jjo);
+	    }
+}
