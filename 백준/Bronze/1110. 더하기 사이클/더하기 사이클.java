6d0a030b94ef2667034686d3bf1a152798175c28
MyStraw <129818873+MyStraw@users.noreply.github.com>
2024-01-30 17:25:31 +0900

    [Bronze I] Title: 더하기 사이클, Time: 212 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int origin = N;
+		int cycle = 0;
+
+		do {
+			int ten = N / 10;
+			int one = N % 10;
+			int sum = ten + one;
+			N = (one * 10) + (sum % 10);
+			cycle++;
+
+		} while (N != origin);
+
+		System.out.println(cycle);
+	}
+}
