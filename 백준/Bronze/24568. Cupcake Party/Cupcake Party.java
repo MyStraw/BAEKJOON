6197480d0c1857a4a2f73663ab2486ff3672439b
MyStraw <129818873+MyStraw@users.noreply.github.com>
2024-02-05 12:43:57 +0900

    [Bronze V] Title: Cupcake Party, Time: 204 ms, Memory: 17564 KB -BaekjoonHub

---
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int R = sc.nextInt();
+		int S = sc.nextInt();
+		
+		int total = R *8 + S * 3;		
+		
+		int howManyLeft = total - 28;
+		
+		System.out.println(howManyLeft);
+		
+	}
+
+}
