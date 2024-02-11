6d67eced574b8b056477e1098fa90551e6ccd806
MyStraw <129818873+MyStraw@users.noreply.github.com>
2024-02-12 06:13:56 +0900

    [Silver V] Title: 다각형의 대각선, Time: 200 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Silver/3049. 다각형의 대각선/다각형의 대각선.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        
+        long cross = combination(N, 4);
+       
+        System.out.println(cross);
+    }
+
+    public static long combination(int n, int k) {
+        long result = 1;
+        for (int i = 0; i < k; i++) {
+            result *= (n - i);
+            result /= (i + 1);
+        }
+        return result;    
+	}
+}
