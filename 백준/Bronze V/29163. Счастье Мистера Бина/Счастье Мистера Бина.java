b47413af7aa115b405f8af904ee76ef3cc2cbeb6
MyStraw <129818873+MyStraw@users.noreply.github.com>
2024-02-15 15:48:35 +0900

    [Bronze V] Title: Счастье Мистера Бина, Time: 276 ms, Memory: 19644 KB -BaekjoonHub

---
+++ 백준/Bronze V/29163. Счастье Мистера Бина/Счастье Мистера Бина.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            int number = sc.nextInt();
+            if (number % 2 == 0) {
+            	count++;
+            }
+        }
+
+        if (count > n - count) {
+            System.out.println("Happy");
+        } else {
+            System.out.println("Sad");
+        }
+    }
+
+}
