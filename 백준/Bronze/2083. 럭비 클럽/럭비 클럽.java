afc1c83de89b1723a0400efc6f7b5b12f7e64f22
MyStraw <129818873+MyStraw@users.noreply.github.com>
2024-02-24 17:11:47 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 228 ms, Memory: 18440 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			String name = sc.next();
+			int age = sc.nextInt(); 
+			int weight = sc.nextInt(); 
+			String classific;
+			
+			if (name.equals("#") && age == 0 && weight == 0) {
+				break; 
+			}
+			
+			if (age > 17 || weight >=80) {
+				classific = "Senior";
+			}
+			else {
+				classific = "Junior";
+			}
+			
+			System.out.println(name + " " + classific);
+		}		
+	}
+}
