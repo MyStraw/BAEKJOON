2924889e6207aacd0014b533185abe311553de06
MyStraw <129818873+MyStraw@users.noreply.github.com>
2024-02-26 15:06:02 +0900

    [Bronze IV] Title: 시험 점수, Time: 116 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/5596. 시험 점수/시험 점수.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st= new StringTokenizer(br.readLine());
+	
+		int minTotal = 0;
+		for (int i= 0; i<4; i++) {
+			minTotal += Integer.parseInt(st.nextToken());
+		}		
+		
+		st = new StringTokenizer(br.readLine());
+		
+		int manTotal = 0;
+		for (int i= 0; i<4; i++) {
+			manTotal += Integer.parseInt(st.nextToken());
+		}
+		
+		if (minTotal >= manTotal) {
+			System.out.println(minTotal);
+		}
+		else {
+			System.out.println(manTotal);
+		}
+				
+	}
+
+}
