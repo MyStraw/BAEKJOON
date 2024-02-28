c071b6c37920210efe59890ddff2096449f7502a
MyStraw <129818873+MyStraw@users.noreply.github.com>
2024-02-28 18:54:41 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 240 ms, Memory: 18736 KB -BaekjoonHub

---
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		int D = sc.nextInt();
+
+		int totalSeconds = C + D + (B * 60) + (A * 3600);
+
+		int endHour = (totalSeconds / 3600) % 24;
+		int endMinute = (totalSeconds % 3600) / 60;
+		int endSecond = totalSeconds % 60;
+
+		System.out.println(endHour + " " + endMinute + " " + endSecond);
+
+	}
+}
