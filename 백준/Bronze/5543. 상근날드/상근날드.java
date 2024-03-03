519d77a7e1a293c1dd0fe948a3c1df3125cd8d56
MyStraw <129818873+MyStraw@users.noreply.github.com>
2024-03-03 13:58:59 +0900

    [Bronze IV] Title: 상근날드, Time: 216 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/5543. 상근날드/상근날드.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int burgerMin = 2000;
+		int tanMin = 2000;
+
+		for (int i = 0; i < 3; i++) {
+			int burger = sc.nextInt();
+			if (burger < burgerMin) {
+				burgerMin = burger;
+			}
+		}
+
+		for (int i = 0; i < 2; i++) {
+			int tansan = sc.nextInt();
+			if (tansan < tanMin) {
+				tanMin = tansan;
+			}
+		}
+		System.out.println(burgerMin + tanMin - 50);
+
+	}
+
+}
