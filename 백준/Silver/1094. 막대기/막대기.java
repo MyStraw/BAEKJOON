597c64ef4bd4d6ff59f036ec97f7ee79cde4f552
MyStraw <129818873+MyStraw@users.noreply.github.com>
2024-03-09 15:38:00 +0900

    [Silver V] Title: 막대기, Time: 212 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int X = sc.nextInt();
+
+		int[] stick = { 64, 32, 16, 8, 4, 2, 1 };
+
+		int count = 0;		
+
+		for (int i = 0; i < 7; i++) {
+			if (X - stick[i] == 0) {
+				count++;
+				break;						
+			} else if (X - stick[i] >= 0) {
+				X = X - stick[i];				
+				count++;				
+			}
+		}
+		System.out.println(count);
+	}
+}
