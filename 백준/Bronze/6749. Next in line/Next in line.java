7b4e6ae5fdeb8e26992394f6b88489c36f7c1267
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-03-19 22:26:59 +0900

    [Bronze IV] Title: Next in line, Time: 200 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int Y = sc.nextInt();
+		int M = sc.nextInt();
+		
+		int first = M + (M-Y);
+		
+		System.out.println(first);
+	}
+}
