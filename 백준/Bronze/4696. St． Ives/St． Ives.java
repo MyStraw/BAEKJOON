eff226ca651cd96f2cebc630d523d8acd522e2e3
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-03-22 21:07:17 +0900

    [Bronze IV] Title: St. Ives, Time: 220 ms, Memory: 17884 KB -BaekjoonHub

---
+++ 백준/Bronze/4696. St． Ives/St． Ives.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			double w = sc.nextDouble();
+			if (w == 0)
+				break;
+			
+			double total = 1 + w + Math.pow(w, 2) + Math.pow(w, 3) + Math.pow(w, 4);
+
+			System.out.printf("%.2f\n", total);
+		}
+	}
+}
