3605116ad46b0922fbdee09be6a4162683d8db5e
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-03-29 20:33:20 +0900

    [Gold V] Title: 내일 할거야, Time: 2392 ms, Memory: 267740 KB -BaekjoonHub

---
+++ 백준/Gold/7983. 내일 할거야/내일 할거야.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[][] N = new int[n][2];
+
+		int last = 0;
+		int gap = 0;
+
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int d = Integer.parseInt(st.nextToken());
+			int t = Integer.parseInt(st.nextToken());
+			N[i][0] = d;
+			N[i][1] = t;
+		}
+		Arrays.sort(N, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o1[1] - o2[1];
+			}
+		});
+
+		for (int i = n - 1; i >= 0; i--) {
+
+			if (last <= N[i][1] && i != n - 1) {
+				gap = N[i][1] - last + 1;
+				last = last - N[i][0];
+			} else
+				last = N[i][1] - N[i][0] + 1;
+
+		}
+		System.out.println(last - 1);
+	}
+}
