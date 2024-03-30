da07f4fbdb6ea44f7653192e93d488dc4108acc5
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-03-30 17:08:30 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 220 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		int count = 0;
+		for (int i = 0; i < N; i++) {
+			Set<Character> cc = new HashSet<>();
+			String group = sc.next();
+			char[] con = group.toCharArray();
+			for (int j = 0; j < con.length - 1; j++) {
+				cc.add(con[j]);
+				if (con[j + 1] == con[j]) {
+					continue;
+				} else if (con[j + 1] != con[j] && !cc.contains(con[j + 1])) {
+					cc.add(con[j + 1]);
+				} else if (con[j + 1] != con[j] && cc.contains(con[j + 1])) {
+					count++;
+					break;
+				}
+			}
+		}
+		System.out.println(N - count);
+	}
+}
