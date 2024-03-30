a6fc6c9f32b04deeafaa07ccf415b5f920d3fa66
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-03-31 01:47:35 +0900

    [Bronze I] Title: 진법 변환 2, Time: 208 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int B = sc.nextInt();
+
+		Stack<Integer> ten = new Stack<>();
+		ten.add(N % B);
+
+		while (N / B != 0) {
+			N = N / B;
+			ten.add(N % B);
+		}
+		int length = ten.size();
+
+		for (int i = 0; i < length; i++) {
+			if (ten.peek() >= 0 && ten.peek() <= 9) {
+				System.out.print(ten.pop());
+			} else {
+				System.out.print((char)(ten.pop() + 'A' - 10));
+			}
+		}
+	}
+}
