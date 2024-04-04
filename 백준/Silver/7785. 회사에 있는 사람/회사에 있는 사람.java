89eb8ad932d0099f8f72086bef316d4e57502e53
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-04-04 14:20:50 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 564 ms, Memory: 42460 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Comparator;
@@ -8,19 +7,16 @@ import java.util.StringTokenizer;
 import java.util.TreeSet;
 
 public class Main {
-	public static void main(String args[]) throws IOException {
+	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
 		int N = Integer.parseInt(br.readLine());
-
 		TreeSet<String> set = new TreeSet<>(new Comparator<String>() {
 			@Override
 			public int compare(String s1, String s2) {
 				return s2.compareTo(s1);
 			}
 		});
-
 		while (N-- > 0) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			String name = st.nextToken();
@@ -32,7 +28,6 @@ public class Main {
 				set.remove(name);
 			}
 		}
-
 		for (String s : set) {
 			bw.write(s);
 			bw.newLine();
