249fe9e0959bc80987a5857c8ac4ebfe6f96e9bc
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-04-07 02:56:55 +0900

    [Silver IV] Title: 행운을 빌어요, Time: 160 ms, Memory: 14904 KB -BaekjoonHub

--- 백준/Silver/31713. 행운을 빌어요/행운을 빌어요.java
+++ 백준/Silver/31713. 행운을 빌어요/행운을 빌어요.java
@@ -1,31 +1,33 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int T = Integer.parseInt(br.readLine());
-
-		int sum = 0;
 		int count = 0;
 		for (int i = 0; i < T; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int A = Integer.parseInt(st.nextToken());
 			int B = Integer.parseInt(st.nextToken());
-
 			if (A * 3 <= B && A * 4 >= B || (A == 0 && B == 0)) {
-				System.out.println(0);
+				bw.write("0\n");
 			} else if (A * 3 > B) {
-				System.out.println(A * 3 - B);
+				bw.write(String.valueOf(A * 3 - B) + "\n");
 			} else if (A * 4 < B) {
-				System.out.println(recur(A, B, sum, count));
+				bw.write(String.valueOf(recur(A, B, count)) + "\n");
 			}
 		}
+		bw.flush();
+		bw.close();
 	}
 
-	public static int recur(int A, int B, int sum, int count) {
+	public static int recur(int A, int B, int count) {
 		int needJul = 0;
 		if (B % 4 != 0) {
 			needJul = B / 4 + 1;
