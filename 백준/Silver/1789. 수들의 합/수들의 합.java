fe8a108b01387ee3fc3f2230a0c0ebe5c36360d0
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-04-09 23:03:51 +0900

    [Silver V] Title: 수들의 합, Time: 128 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Silver/1789. 수들의 합/수들의 합.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		long S = Long.parseLong(br.readLine());
+
+		long total = 0;
+		int count = 0;		
+
+		for (int i = 1; total + i <= S; i++) {
+			total += i;
+			count++;			
+		}
+		System.out.println(count);
+	}
+}
