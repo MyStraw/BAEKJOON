6c4c308ff0429fc2612e91efa3ad4af951290b0f
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-04-11 21:01:50 +0900

    [Silver I] Title: 회의실 배정, Time: 532 ms, Memory: 44424 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -19,33 +20,37 @@ public class Main {
 			meetings[i] = new Meeting(start, end);
 		}
 
-		Arrays.sort(meetings); 
+		Arrays.sort(meetings, new Comparator<Meeting>() {
+			@Override
+			public int compare(Meeting o1, Meeting o2) {
+				if (o1.end == o2.end) {
+					return o1.start - o2.start;
+				}
+				return o1.end - o2.end;
+			}
+		});
+
 		int count = 0;
 		int prevEndTime = 0;
 
 		for (int i = 0; i < N; i++) {			
 			if (meetings[i].start >= prevEndTime) {
-				prevEndTime = meetings[i].end;
-				count++;
+				prevEndTime = meetings[i].end; 
+				count++; 
 			}
 		}
+
 		System.out.println(count);
+
 	}
-}
 
-class Meeting implements Comparable<Meeting> {
-	int start;
-	int end;
+	static class Meeting {
+		int start;
+		int end;
 
-	public Meeting(int start, int end) {
-		this.start = start;
-		this.end = end;
-	}	
-	@Override
-	public int compareTo(Meeting o) {
-		if (this.end == o.end) {
-			return this.start - o.start;
+		public Meeting(int start, int end) {
+			this.start = start;
+			this.end = end;
 		}
-		return this.end - o.end;
 	}
 }
