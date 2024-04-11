0adcd9c1428c0119796532b0796b29722c5aee46
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-04-12 02:28:40 +0900

    [Gold V] Title: 내일 할거야, Time: 2316 ms, Memory: 272088 KB -BaekjoonHub

--- 백준/Gold/7983. 내일 할거야/내일 할거야.java
+++ 백준/Gold/7983. 내일 할거야/내일 할거야.java
@@ -2,41 +2,58 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int[][] N = new int[n][2];
 
-		int last = 0;
-		int gap = 0;
+		Nail doit[] = new Nail[n];
 
 		for (int i = 0; i < n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int d = Integer.parseInt(st.nextToken());
-			int t = Integer.parseInt(st.nextToken());
-			N[i][0] = d;
-			N[i][1] = t;
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int term = Integer.parseInt(st.nextToken());
+			int limit = Integer.parseInt(st.nextToken());
+			doit[i] = new Nail(term, limit);
 		}
-		Arrays.sort(N, new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o1[1] - o2[1];
-			}
-		});
 
+		Arrays.sort(doit);
+
+		int last = 0;
 		for (int i = n - 1; i >= 0; i--) {
-
-			if (last <= N[i][1] && i != n - 1) {
-				gap = N[i][1] - last + 1;
-				last = last - N[i][0];
+			if (last <= doit[i].getLimit() && i != n - 1) {
+				last = last - doit[i].getTerm();
 			} else
-				last = N[i][1] - N[i][0] + 1;
-
+				last = doit[i].getLimit() - doit[i].getTerm() + 1;
 		}
 		System.out.println(last - 1);
+
 	}
+
+	static class Nail implements Comparable<Nail> {
+		private int term;
+		private int limit;
+
+		public Nail(int term, int limit) {
+			this.term = term;
+			this.limit = limit;
+		}
+
+		public int compareTo(Nail o) {
+			return this.limit - o.limit;
+
+		}
+
+		public int getTerm() {
+			return term;
+		}
+
+		public int getLimit() {
+			return limit;
+		}
+
+	}
+
 }
