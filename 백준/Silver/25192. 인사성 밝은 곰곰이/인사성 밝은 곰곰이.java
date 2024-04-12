59771283db49eea538a2051814f65dc57222524a
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-04-12 18:03:46 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 296 ms, Memory: 25348 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		HashSet<String> hash = new HashSet<>();
+		int count = 0;
+		boolean enter = false;
+
+		for (int i = 0; i < n; i++) {
+			String nick = br.readLine();
+			if (nick.equals("ENTER")) {
+				hash.clear();
+				enter = true;
+			} else if (true && !hash.contains(nick)) {
+				hash.add(nick);
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
