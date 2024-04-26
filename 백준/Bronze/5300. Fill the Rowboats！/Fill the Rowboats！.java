8e33c6307647026b85fd05f7643824c8d6ab50cc
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-04-26 17:57:19 +0900

    [Bronze IV] Title: Fill the Rowboats!, Time: 200 ms, Memory: 17456 KB -BaekjoonHub

---
+++ 백준/Bronze/5300. Fill the Rowboats！/Fill the Rowboats！.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 1; i <= N; i++) {
+			sb.append(i).append(" ");
+
+			if (i % 6 == 0) {
+				sb.append("Go! ");
+
+			}
+		}
+
+		if (N % 6 != 0) {
+			sb.append("Go!");
+		}
+
+		System.out.println(sb.toString().trim());
+	}
+}
