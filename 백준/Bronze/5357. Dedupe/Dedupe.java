3fe395ea1be218bb9af0a2c93d07159511189bf6
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-04-27 13:10:38 +0900

    [Bronze IV] Title: Dedupe, Time: 208 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/5357. Dedupe/Dedupe.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			String str = sc.next();
+			StringBuilder sb = new StringBuilder();
+
+			sb.append(str.charAt(0));
+
+			for (int j = 1; j < str.length(); j++) {
+				if (str.charAt(j) != str.charAt(j - 1)) {
+					sb.append(str.charAt(j));
+				}
+			}
+			System.out.println(sb.toString());
+		}
+	}
+}
