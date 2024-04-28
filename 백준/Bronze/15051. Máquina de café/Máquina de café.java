6c04f9cb7b549734ad72ce93438f9c09be8768bc
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-04-28 14:57:42 +0900

    [Bronze IV] Title: Máquina de café, Time: 208 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/15051. Máquina de café/Máquina de café.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int A1 = sc.nextInt(); 
+		int A2 = sc.nextInt(); 
+		int A3 = sc.nextInt(); 
+
+		
+		int f1 = (A2 * 2) + (A3 * 4);
+
+		
+		int f2 = (A1 * 2) + (A3 * 2);
+
+	
+		int f3 = (A1 * 4) + (A2 * 2);
+
+		
+		int min = Math.min(f1, Math.min(f2, f3));
+
+		System.out.println(min); 
+
+	
+	}
+
+}
