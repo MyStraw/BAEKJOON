b7d2ac075e6910c6dfa1509e505ee250791ceff7
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-05-01 16:15:06 +0900

    [Bronze IV] Title: Silnia, Time: 212 ms, Memory: 17580 KB -BaekjoonHub

---
+++ 백준/Bronze/8558. Silnia/Silnia.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		if (n == 0) {
+			System.out.println(1); 
+		} else {
+			int il = 1;
+			for (int i = 1; i <= n; i++) {
+				il *= i;
+				il %= 10;
+				if (il == 0)
+					break; 
+			}
+			System.out.println(il);
+		}
+
+	}
+
+}
