6a5b2693d5387caa9226a819588ad83f40b9e486
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-05-05 15:11:53 +0900

    [Bronze IV] Title: Sounds fishy!, Time: 204 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/6764. Sounds fishy！/Sounds fishy！.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] finder = new int[4];
+
+		for (int i = 0; i < 4; i++) {
+			finder[i] = sc.nextInt();
+		}
+
+		if (finder[0] < finder[1] && finder[1] < finder[2] && finder[2] < finder[3]) {
+			System.out.println("Fish Rising");
+		} else if (finder[0] > finder[1] && finder[1] > finder[2] && finder[2] > finder[3]) {
+			System.out.println("Fish Diving");
+		} else if (finder[0] == finder[1] && finder[1] == finder[2] && finder[2] == finder[3]) {
+			System.out.println("Fish At Constant Depth");
+		} else {
+			System.out.println("No Fish");
+		}
+
+	}
+
+}
