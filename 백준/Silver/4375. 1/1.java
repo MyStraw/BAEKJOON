d0ce420206819a5483d51548054a8c199d3523c1
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-06-03 11:11:35 +0900

    [Silver III] Title: 1, Time: 208 ms, Memory: 17480 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.java
+++ 백준/Silver/4375. 1/1.java
@@ -1,22 +1,18 @@
-import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		while (sc.hasNextInt()) {
+			while (sc.hasNextInt()) {
 			int n = sc.nextInt();
-			BigInteger num = BigInteger.ONE;
-			int count = 1; 
-			while (true) {
-				if (num.mod(BigInteger.valueOf(n)).equals(BigInteger.ZERO)) {
-					System.out.println(count);
-					break;
-				}
-				num = num.multiply(BigInteger.TEN).add(BigInteger.ONE);
-				count++;
-			}
+			long i = 1;
+			int count = 1;
+		    while (i % n != 0) {
+	            i = (i * 10 + 1) % n; // 나머지 연산을 사용하여 큰 수를 다루지 않음
+	            count++; // 자릿수 증가
+	        }
+			System.out.println(count);
 		}
 	}
 }
