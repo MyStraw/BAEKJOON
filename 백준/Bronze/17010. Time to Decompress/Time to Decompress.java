6a3e0c4cbf525bff675c9ab955b8bf9faca105fc
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-06-08 20:18:51 +0900

    [Bronze IV] Title: Time to Decompress, Time: 216 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/17010. Time to Decompress/Time to Decompress.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int L = sc.nextInt();
+		sc.nextLine();
+
+		for (int i = 0; i < L; i++) {
+			int N = sc.nextInt();
+			char C = sc.next().charAt(0);
+
+			for (int j = 0; j < N; j++) {
+				System.out.print(C);
+			}
+
+			System.out.println();
+		}
+	}
+}
