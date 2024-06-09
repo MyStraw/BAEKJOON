8ecc397fb5af13c9d8966b2c8ea5fafb7fd66dd8
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-06-09 09:15:23 +0900

    [Bronze I] Title: 3개만!, Time: 204 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/17288. 3개만！/3개만！.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String S = sc.next();
+
+		int count = 0;
+		for (int i = 0; i < S.length() - 2; i++) {
+			if (S.charAt(i + 1) == S.charAt(i) + 1 && S.charAt(i + 2) == S.charAt(i) + 2) {
+				if (i + 3 >= S.length() || S.charAt(i + 3) != S.charAt(i) + 3) {
+                    count++;                  
+                    i += 2;
+                } else {                    
+                    while (i + 3 < S.length() && S.charAt(i + 3) == S.charAt(i) + 3) {
+                        i++;
+                    }
+                }	
+			}
+		}		
+
+		System.out.println(count);
+	}
+}
