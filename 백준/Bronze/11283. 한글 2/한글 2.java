8a74dfead9178594ccf5989114ed6d1334b7a018
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-06-19 15:22:37 +0900

    [Bronze IV] Title: 한글 2, Time: 208 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char han = sc.next().charAt(0);
+
+		int result = getIndex(han);
+		System.out.println(result);
+
+	}
+
+	public static int getIndex(char han) {
+		int uniCode = 0xAC00; // "가"의 유니코드
+		int code = han - uniCode;
+
+		int jongIdx = code % 28;
+		int jungIdx = (code / 28) % 21;
+		int choIdx = (code / 28) / 21;
+
+		return choIdx * 21 * 28 + jungIdx * 28 + jongIdx + 1;
+	}
+
+}
