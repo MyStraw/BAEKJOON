a499f76d6eadd38f49f5110680785c43394650a8
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-07-01 21:21:45 +0900

    [Silver IV] Title: 시파르, Time: 208 ms, Memory: 18516 KB -BaekjoonHub

---
+++ 백준/Silver/9693. 시파르/시파르.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int caseN = 1;
+
+		while (true) {
+			int N = sc.nextInt();
+			if (N == 0)
+				break;
+
+			int result = zeroPPaegi(N);
+			System.out.println("Case #" + caseN + ": " + result);
+			caseN++;
+		}
+
+	}
+
+	public static int zeroPPaegi(int N) {
+		int count = 0;
+		for (int i = 5; N / i >= 1; i *= 5) {
+			count += N / i;
+		}
+		return count;
+	}
+
+}
