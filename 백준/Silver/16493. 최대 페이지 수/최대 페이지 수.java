3d2a288c4c6da06c92794ceef8074ade1bba29b8
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-07-03 03:38:51 +0900

    [Silver II] Title: 최대 페이지 수, Time: 212 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Silver/16493. 최대 페이지 수/최대 페이지 수.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+
+		int[] day = new int[M];
+		int[] page = new int[M];
+
+		for (int i = 0; i < M; i++) {
+			day[i] = sc.nextInt();
+			page[i] = sc.nextInt();
+		}
+
+		int[] dp = new int[N + 1];
+
+		for (int i = 0; i < M; i++) {
+			for (int j = N; j >= day[i]; j--) {
+				dp[j] = Math.max(dp[j], dp[j - day[i]] + page[i]);
+			}
+		}
+
+		System.out.println(dp[N]);
+
+	}
+
+}
