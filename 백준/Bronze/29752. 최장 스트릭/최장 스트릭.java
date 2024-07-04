fcbb58cdc31d48665a96c58e54608659e619fd04
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-07-04 14:12:02 +0900

    [Bronze III] Title: 최장 스트릭, Time: 128 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Bronze/29752. 최장 스트릭/최장 스트릭.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int streak[] = new int[N];
+
+		int max = 0;
+		int count = 0;
+
+		for (int i = 0; i < N; i++) {
+			streak[i] = Integer.parseInt(st.nextToken());
+			if (streak[i] != 0) {
+				count++;
+			} else {
+				count = 0;
+			}
+			if (max < count) {
+				max = count;
+			}
+		}
+		System.out.println(max);
+
+	}
+
+}
