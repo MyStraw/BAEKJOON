fff9b53a48032c309ac00f4d751636f9a5fccd8a
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-07-25 10:13:37 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 208 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+
+		String[] minus = str.split("-");
+
+		int result = 0;
+
+		for (int i = 0; i < minus.length; i++) {
+			String part = minus[i];
+			String[] plus = part.split("\\+");
+
+			int sum = 0;
+			for (String num : plus) {
+				sum += Integer.parseInt(num);
+			}
+
+			if (i == 0) {
+				result += sum;
+			} else {
+				result -= sum;
+			}
+		}
+
+		System.out.println(result);
+
+	}
+
+}
