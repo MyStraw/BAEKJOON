812ba06962272bae2880c0c759267ad1eb425e9d
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-08-05 10:06:00 +0900

    [Gold V] Title: 더하기와 나누기, Time: 292 ms, Memory: 21656 KB -BaekjoonHub

---
+++ 백준/Gold/28426. 더하기와 나누기/더하기와 나누기.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int N = scanner.nextInt();
+		scanner.close();
+
+		StringBuilder result = new StringBuilder();
+		int i = 2;
+		
+		result.append("3 ");
+		
+		for (int _n = N - 1; --_n > 0; i += 2) {
+			result.append(i).append(" ");
+		}
+		if (N > 1) {
+			result.append(i + (N % 3 == 2 ? 4 : 0));
+		}
+		
+		System.out.println(result.toString().trim());
+	}
+
+}
