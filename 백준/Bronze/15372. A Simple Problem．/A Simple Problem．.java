517b3a87f7e237b91f3177cf2ad7d6a06db6d58d
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-08-11 15:55:07 +0900

    [Bronze IV] Title: A Simple Problem., Time: 464 ms, Memory: 35768 KB -BaekjoonHub

---
+++ 백준/Bronze/15372. A Simple Problem．/A Simple Problem．.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < T; i++) {		
+			int N = Integer.parseInt(br.readLine());
+		
+			long K = (long) N * N;
+			sb.append(K).append("\n");
+		}
+	
+		System.out.print(sb.toString());
+	}
+
+}
