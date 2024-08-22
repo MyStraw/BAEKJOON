a9b714758a6fa1d8a34eb22f36a29f4888b831b7
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-08-22 16:11:06 +0900

    [Bronze I] Title: 벼락치기, Time: 104 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/23739. 벼락치기/벼락치기.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] thunder = new int[N];
+	
+		int ans = 0;
+		int sum = 0;	
+
+		for (int i = 0; i < N; i++) {
+			thunder[i] = Integer.parseInt(br.readLine());
+			
+			if ((thunder[i] + 1) / 2 <= 30 - sum) {
+				ans++;
+			}
+			sum += thunder[i];
+			if (sum >= 30) {
+				sum = 0;
+			}
+		}	
+		System.out.println(ans);
+	}
+
+}
