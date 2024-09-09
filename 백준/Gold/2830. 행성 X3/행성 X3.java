f9c4d7a26d4dd2bf90addf85a2de605a7ec1ab00
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-09-09 20:09:49 +0900

    [Gold III] Title: 행성 X3, Time: 340 ms, Memory: 68172 KB -BaekjoonHub

---
+++ 백준/Gold/2830. 행성 X3/행성 X3.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int[] residents = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			residents[i] = Integer.parseInt(br.readLine());
+		}
+
+		long totalValue = 0;
+
+		for (int bit = 0; bit < 30; bit++) {
+			long countOne = 0;
+			
+			for (int i = 0; i < N; i++) {
+				if ((residents[i] & (1 << bit)) != 0) {
+					countOne++;
+				}
+			}
+			
+			long countZero = N - countOne;
+			
+			totalValue += countOne * countZero * (1 << bit);
+		}
+
+		System.out.println(totalValue);
+	}
+
+}
