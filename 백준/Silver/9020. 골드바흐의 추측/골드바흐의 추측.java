c2c0a9010b130a42d71fd0dc2d6eeb7d4dd0ba57
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-09-19 16:26:29 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 740 ms, Memory: 36136 KB -BaekjoonHub

---
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+	static int T, num;
+
+	static boolean isPrimeNum(int num) {
+		for (int i = 2; i < num; i++) {
+			if (num % i == 0)
+				return false;
+		}
+		return true;
+	}
+
+	static void Answer(int num) {
+		int length = num / 2;
+
+		for (int i = 0; i < length; i++) {
+			int temp1 = length - i;
+			if (isPrimeNum(temp1)) {
+				if (isPrimeNum(num - temp1)) {
+					System.out.println(temp1 + " " + (num - temp1));
+					return;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		T = sc.nextInt();
+		for (int i = 0; i < T; i++) {
+			num = sc.nextInt();			
+			Answer(num);
+		}	
+	}
+}
