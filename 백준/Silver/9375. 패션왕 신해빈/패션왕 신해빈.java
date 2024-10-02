001db85e616c0071a4776ce5fc51ec268d89e265
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-10-02 10:15:29 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 100 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
+		while (T-- > 0) {
+			int n = Integer.parseInt(br.readLine());
+			Map<String, Integer> clothes = new HashMap<>();
+
+			for (int i = 0; i < n; i++) {
+				String[] input = br.readLine().split(" ");
+				String type = input[1];
+				clothes.put(type, clothes.getOrDefault(type, 0) + 1);
+			}
+
+			long result = 1;
+			for (int count : clothes.values()) {
+				result *= (count + 1);
+			}
+
+			sb.append(result - 1).append("\n");
+		}
+		System.out.print(sb);		
+	}
+
+}
