741a8e67cc336d6933fd38dea5791b1dd8a454d8
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-10-07 09:23:10 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 100 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		for (int i = 0; i < N; i++) {
+			String boong = br.readLine();			
+			StringBuilder sb = new StringBuilder(boong);
+			String reverse = sb.reverse().toString();
+			System.out.println(reverse);
+		}
+
+	}
+
+}
