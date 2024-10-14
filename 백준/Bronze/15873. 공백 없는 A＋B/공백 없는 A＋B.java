802bbe57f326cd4bbaf54301f7b5d5a5a6b484cf
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-10-14 13:34:33 +0900

    [Bronze IV] Title: 공백 없는 A+B, Time: 100 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/15873. 공백 없는 A＋B/공백 없는 A＋B.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		int A, B;
+
+		if (input.length() == 2) {
+			A = Character.getNumericValue(input.charAt(0));
+			B = Character.getNumericValue(input.charAt(1));
+		} else if (input.length() == 3) {
+			if (input.startsWith("10")) {
+				A = 10;
+				B = Character.getNumericValue(input.charAt(2));
+			} else {
+				A = Character.getNumericValue(input.charAt(0));
+				B = 10;
+			}
+		} else {
+			A = 10;
+			B = 10;
+		}
+
+		System.out.println(A + B);
+	}
+}
