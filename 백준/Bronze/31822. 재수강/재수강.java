fb9feb870eff0c127350a6cf5dca64284ffe70c2
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-10-23 12:54:36 +0900

    [Bronze IV] Title: 재수강, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/31822. 재수강/재수강.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String jae = br.readLine();
+
+		String ho = jae.substring(0, 5);	
+
+		int N = Integer.parseInt(br.readLine());
+
+		int count = 0;
+		String[] can = new String[N];
+		for (int i = 0; i < N; i++) {
+			can[i] = br.readLine();
+			if (can[i].startsWith(ho)) {
+				count++;
+			}
+
+		}
+		System.out.println(count);
+
+	}
+
+}
