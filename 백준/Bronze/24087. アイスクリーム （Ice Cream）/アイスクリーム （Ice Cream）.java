19307b7531b9d81111280eade263bddd2991cc72
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-10-30 10:35:06 +0900

    [Bronze IV] Title: アイスクリーム (Ice Cream), Time: 104 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/24087. アイスクリーム （Ice Cream）/アイスクリーム （Ice Cream）.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int S = Integer.parseInt(br.readLine());
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+
+		int basePrice = 250;
+		int additionalPrice = 100;
+
+		if (A >= S) {
+			System.out.println(basePrice);
+		} else {
+			int additionalIceCreams = (S - A + B - 1) / B;
+			int totalPrice = basePrice + additionalIceCreams * additionalPrice;
+			System.out.println(totalPrice);
+		}
+	}
+
+}
