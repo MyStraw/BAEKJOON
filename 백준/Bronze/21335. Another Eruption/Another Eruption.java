9777fa745ac299bcd6289dba3caf94dbc34ba9fa
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-11-11 13:13:31 +0900

    [Bronze IV] Title: Another Eruption, Time: 116 ms, Memory: 14608 KB -BaekjoonHub

---
+++ 백준/Bronze/21335. Another Eruption/Another Eruption.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		double area = Double.parseDouble(br.readLine());	
+		double radius = Math.sqrt(area / Math.PI);	
+		double circumference = 2 * Math.PI * radius;	
+		System.out.println(String.format("%.9f", circumference));	
+	}
+}
