598138b4b1874a7c84de6f707d2d2e5ea0dead91
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-11-13 13:22:47 +0900

    [Bronze III] Title: 수학적 호기심, Time: 604 ms, Memory: 25248 KB -BaekjoonHub

---
+++ 백준/Bronze/9094. 수학적 호기심/수학적 호기심.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int t = 0; t < T; t++) {
+			String[] input = br.readLine().split(" ");
+			int n = Integer.parseInt(input[0]);
+			int m = Integer.parseInt(input[1]);
+
+			int count = 0;		
+			for (int a = 1; a < n; a++) {
+				for (int b = a + 1; b < n; b++) {				
+					if ((1L * a * a + 1L * b * b + m) % (1L * a * b) == 0) {
+						count++;
+					}
+				}
+			}
+
+			System.out.println(count);
+		}	
+	}
+}
