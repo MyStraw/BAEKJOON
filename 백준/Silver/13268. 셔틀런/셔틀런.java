27d32210a589c9c093d941b9efd1b0b03bbc4ab3
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-11-19 14:53:26 +0900

    [Silver III] Title: 셔틀런, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/13268. 셔틀런/셔틀런.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int remain = n;
+		int d = 0;
+
+		while (remain > 0) {
+			d = 0;
+			for (int i = 1; i <= 4; i++) {
+
+				for (int j = 0; j < i; j++) {
+					remain -= 5;
+					if (remain <= 0) {
+						d += 5 + remain;
+						break;
+					}
+					d += 5;
+				}
+				if (remain <= 0)
+					break;
+
+				for (int j = 0; j < i; j++) {
+					remain -= 5;
+					if (remain <= 0) {
+						d -= 5 + remain;
+						break;
+					}
+					d -= 5;
+				}
+				if (remain <= 0)
+					break;
+			}
+			if (remain <= 0)
+				break;
+		}
+
+		System.out.println(d % 5 == 0 ? d / 5 : d / 5 + 1);
+	}
+
+}
