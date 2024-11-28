c6b4791a858c507eaffaf2f5835c6df73d481908
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-11-28 16:30:50 +0900

    [Bronze II] Title: ZOAC 2, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/18238. ZOAC 2/ZOAC 2.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		int time = 0;
+		char current = 'A';
+
+		for (char c : s.toCharArray()) {
+			int clockwise = Math.abs(c - current);
+			int counterClockwise = 26 - clockwise;
+			time += Math.min(clockwise, counterClockwise);
+			current = c;
+		}
+
+		System.out.println(time);
+	}
+
+}
