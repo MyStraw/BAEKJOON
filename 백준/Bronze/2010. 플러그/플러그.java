687055e7cc9807ee7c3d23754195a216e4599f5d
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-12-04 13:56:46 +0900

    [Bronze III] Title: 플러그, Time: 240 ms, Memory: 41612 KB -BaekjoonHub

---
+++ 백준/Bronze/2010. 플러그/플러그.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int total = 0;
+		for (int i = 0; i < n; i++) {
+			int plugs = Integer.parseInt(br.readLine());
+			total += plugs;
+		}
+		System.out.println(total - (n - 1));
+	}
+
+}
