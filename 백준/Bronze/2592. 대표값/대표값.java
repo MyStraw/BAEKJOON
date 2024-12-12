daa2c0b3ee413ad07a80cac3b1f6c2b4a1257a13
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-12-12 19:33:28 +0900

    [Bronze II] Title: 대표값, Time: 104 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/2592. 대표값/대표값.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] nums = new int[10];
+		int sum = 0;
+		HashMap<Integer, Integer> freq = new HashMap<>();
+
+		for (int i = 0; i < 10; i++) {
+			nums[i] = Integer.parseInt(br.readLine());
+			sum += nums[i];
+			freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
+		}
+
+		int avg = sum / 10;
+		int mode = nums[0];
+		int maxCount = 0;
+
+		for (int key : freq.keySet()) {
+			if (freq.get(key) > maxCount) {
+				maxCount = freq.get(key);
+				mode = key;
+			}
+		}
+
+		System.out.println(avg);
+		System.out.println(mode);
+	}
+
+}
