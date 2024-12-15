8effa2356da52f5e724b4a15cc1bb8af0c2df435
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-12-15 11:27:42 +0900

    [Silver III] Title: 삼각, Time: 108 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Silver/15735. 삼각/삼각.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[10001];
+        for (int i = 1; i < 10001; i++) {
+            arr[i] = arr[i - 1] + i;
+        }
+
+		int n = Integer.parseInt(br.readLine()); 
+        long ans = 0;
+
+        for (int i = 1; i <= n; i++) {
+            ans += arr[i];
+        }
+
+        for (int i = n - 1; i > 0; i -= 2) {
+            ans += arr[i]; 
+        }
+
+        System.out.println(ans); 
+    }
+
+}
