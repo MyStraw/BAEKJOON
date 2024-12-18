d1d2be1be84e64e2dab9c7b29b9becee66837bd9
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-12-18 12:58:49 +0900

    [Bronze II] Title: 줄임말 만들기, Time: 108 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/3181. 줄임말 만들기/줄임말 만들기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] ignore = { "i", "pa", "te", "ni", "niti", "a", "ali", "nego", "no", "ili" };
+		String input = br.readLine();
+
+		StringTokenizer st = new StringTokenizer(input);
+		StringBuilder sb = new StringBuilder();
+		boolean first = true;
+
+		while (st.hasMoreTokens()) {
+			String word = st.nextToken();
+			if (first || !contains(ignore, word)) {
+				sb.append(Character.toUpperCase(word.charAt(0)));
+				first = false;
+			}
+		}
+
+		System.out.println(sb.toString());
+	}
+
+	private static boolean contains(String[] arr, String target) {
+		for (String s : arr) {
+			if (s.equals(target)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
