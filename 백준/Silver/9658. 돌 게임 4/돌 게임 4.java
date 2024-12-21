2bb7e26e0ab00588f8f7b42beefcf24520382ca9
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-12-21 13:15:40 +0900

    [Silver II] Title: 돌 게임 4, Time: 112 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		if (n == 1) {
+			System.out.println("CY");
+			return;
+		}
+
+		boolean[] dp = new boolean[Math.max(5, n + 1)];
+
+		dp[2] = true;
+		dp[4] = true;
+
+		for (int i = 5; i <= n; i++) {
+			if (!dp[i - 1] || !dp[i - 3] || !dp[i - 4]) {
+				dp[i] = true;
+			}
+		}
+
+		System.out.println(dp[n] ? "SK" : "CY");
+	}
+}
