9a53b22cca032373973ff075231321bafb0ac755
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2024-12-30 14:42:24 +0900

    [Silver I] Title: 센티와 마법의 뿅망치, Time: 344 ms, Memory: 27744 KB -BaekjoonHub

---
+++ 백준/Silver/19638. 센티와 마법의 뿅망치/센티와 마법의 뿅망치.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] firstLine = br.readLine().split(" ");
+		int n = Integer.parseInt(firstLine[0]);
+		int h = Integer.parseInt(firstLine[1]);
+		int t = Integer.parseInt(firstLine[2]);
+
+		PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a); // 최대 힙
+
+		for (int i = 0; i < n; i++) {
+			pq.add(Integer.parseInt(br.readLine()));
+		}
+
+		int cnt = 0;
+
+		while (t > 0) {
+			int tallest = pq.poll();
+
+			if (tallest < h || tallest == 1) {
+				pq.add(tallest);
+				break;
+			}
+
+			pq.add(tallest / 2);
+			cnt++;
+			t--;
+		}
+
+		if (pq.peek() >= h) {
+			System.out.println("NO");
+			System.out.println(pq.peek());
+		} else {
+			System.out.println("YES");
+			System.out.println(cnt);
+		}
+	}
+
+}
