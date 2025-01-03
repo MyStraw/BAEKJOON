bb2f8347bcaa2e9ecc80ac8b7cc0eb41ffa160c2
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2025-01-03 17:25:41 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" ");
+		long N = Long.parseLong(input[0]);
+		long M = Long.parseLong(input[1]);
+		System.out.println((N * M) / 2);
+	}
+
+}
