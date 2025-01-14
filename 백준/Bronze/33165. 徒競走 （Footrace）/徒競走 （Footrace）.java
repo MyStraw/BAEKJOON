36d0f155e6c1b7dfc72c3cd42a404ba219dfda59
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2025-01-14 14:31:03 +0900

    [Bronze V] Title: 徒競走 (Footrace), Time: 172 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/33165. 徒競走 （Footrace）/徒競走 （Footrace）.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		int V = sc.nextInt();
+		System.out.println(T * V);
+	}
+
+}
