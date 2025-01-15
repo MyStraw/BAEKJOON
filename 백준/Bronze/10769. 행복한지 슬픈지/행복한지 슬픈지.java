b1442f1468a7a05a402f2d4c85b2daf4da985818
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2025-01-15 12:23:55 +0900

    [Bronze I] Title: 행복한지 슬픈지, Time: 100 ms, Memory: 14020 KB -BaekjoonHub

---
+++ 백준/Bronze/10769. 행복한지 슬픈지/행복한지 슬픈지.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+
+		int happyCount = 0;
+		int sadCount = 0;
+
+		for (int i = 0; i < input.length() - 2; i++) {
+			String emoticon = input.substring(i, i + 3);
+			if (emoticon.equals(":-)")) {
+				happyCount++;
+			} else if (emoticon.equals(":-(")) {
+				sadCount++;
+			}
+		}
+
+		if (happyCount == 0 && sadCount == 0) {
+			System.out.println("none");
+		} else if (happyCount == sadCount) {
+			System.out.println("unsure");
+		} else if (happyCount > sadCount) {
+			System.out.println("happy");
+		} else {
+			System.out.println("sad");
+		}
+	}
+
+}
