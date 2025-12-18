a3582b57e0f8c9a232807c3e9a2ec6553b5e1ca2
Jeong SeungGil <129818873+MyStraw@users.noreply.github.com>
2025-12-18 21:51:13 +0900

    [Bronze IV] Title: 가희와 클럽 오디션 1, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30794. 가희와 클럽 오디션 1/가희와 클럽 오디션 1.py
@@ -0,0 +1,15 @@
+import sys
+
+lv, judge = sys.stdin.readline().split()
+lv = int(lv)
+
+if judge == "miss":
+    print(0)
+elif judge == "bad":
+    print(200 * lv)
+elif judge == "cool":
+    print(400 * lv)
+elif judge == "great":
+    print(600 * lv)
+else:
+    print(1000 * lv)
